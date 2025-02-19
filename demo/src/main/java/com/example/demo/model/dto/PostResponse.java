7134c0e6863c495ecd3027fbf5d255fb5f2bd3f5
yujin-Kim-98 <yujin123.kim@gmail.com>
2025-02-19 19:43:20 +0900

    실기 수업 - 어쨌든 테스트 코드 초기 코드

---
+++ demo/src/main/java/com/example/demo/model/dto/PostResponse.java
@@ -0,0 +1,16 @@
+package com.example.demo.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostResponse {
+
+    private Long id;
+    private String content;
+    private Long createdAt;
+    private Long modifiedAt;
+    private UserResponse writer;
+
+}
