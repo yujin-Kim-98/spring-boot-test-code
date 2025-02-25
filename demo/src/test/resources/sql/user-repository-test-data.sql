203b9fe76b05f58af0d7394b216030e2f1a58a76
yujin-Kim-98 <yujin123.kim@gmail.com>
2025-02-25 17:32:42 +0900

    test : userService read 메소드 테스트 코드 추가

--- demo/src/test/resources/sql/user-repository-test-data.sql
+++ demo/src/test/resources/sql/user-repository-test-data.sql
@@ -1,2 +1,5 @@
 insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
 values ('1', 'yujin123.kim@gmail.com', 'yujin123', 'seoul', 'aaaaaaaaaaaaaaa', 'ACTIVE', 0);
+
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values ('2', 'yujin1234.kim@gmail.com', 'yujin1234', 'seoul', 'aaaaaaaaaaaaaaada', 'PENDING', 0);
