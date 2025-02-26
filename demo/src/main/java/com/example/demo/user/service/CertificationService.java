b07e3ba594fd6bfac4ea0a88a3b3e927da65403c
yujin-Kim-98 <yujin123.kim@gmail.com>
2025-02-26 19:07:45 +0900

    test : CertificationService 테스트 추가

--- demo/src/main/java/com/example/demo/user/service/CertificationService.java
+++ demo/src/main/java/com/example/demo/user/service/CertificationService.java
@@ -2,8 +2,6 @@ package com.example.demo.user.service;
 
 import com.example.demo.user.service.port.MailSender;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,14 +11,11 @@ public class CertificationService {
     private final MailSender mailSender;
 
     public void send(String email, long userId, String certificationCode) {
-
         String certificationUrl = generateCertificationUrl(userId, certificationCode);
         String title = "Please certify your email address";
         String content = "Please click the following link to certify your email address: " + certificationUrl;
 
         mailSender.send(email, title, content);
-
-
     }
 
     private String generateCertificationUrl(long userId, String certificationCode) {
---
+++ demo/src/test/java/com/example/demo/user/service/CertificationServiceTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.user.service;
+
+import com.example.demo.mock.FakeMailSender;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CertificationServiceTest {
+
+    @Test
+    public void 이메일과_컨텐츠가_제대로_만들어져서_보내지는지_테스트한다() {
+        // given
+        FakeMailSender fakeMailSender = new FakeMailSender();
+        CertificationService certificationService = new CertificationService(fakeMailSender);
+
+        // when
+        certificationService.send("yujin123.kim@gmail.com", 1, "aaaaaaaaaaaaaaada");
+
+        // then
+        assertThat(fakeMailSender.email).isEqualTo("yujin123.kim@gmail.com");
+        assertThat(fakeMailSender.title).isEqualTo("Please certify your email address");
+        assertThat(fakeMailSender.content).isEqualTo(
+                "Please click the following link to certify your email address: http://localhost:8000/api/users/1/verify?certificationCode=aaaaaaaaaaaaaaada"
+        );
+    }
+
+}
